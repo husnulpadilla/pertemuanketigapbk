36329bd22695bc7166d086d0347f7f1f2f5392eb
husnulpadilla <husnulpadilla@gmail.com>
2025-06-22 20:39:56 +0700

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-footer class="bg-grey-8 text-white q-pa-md" elevated>
+    <div class="text-center">&copy; 2025 Toko Online</div>
+  </q-footer>
+</template>
+
+<script setup>
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-header elevated class="bg-primary text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        Toko Online
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup>
+</script>
